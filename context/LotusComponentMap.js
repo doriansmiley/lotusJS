589dc0273f22de29ead424841a05a1b925b105df
dorian smiley <doriansmiley@siliconpublishing.com>
2014-11-10 16:01:34 -0800

    updating component map

--- context/LotusComponentMap.js
+++ context/LotusComponentMap.js
@@ -7,7 +7,6 @@ Lotus.ComponentMap = function(){
 }
 
 Lotus.ComponentMap.prototype.mapComponent = function( tagName, extendsTag, functionConstructor ){
-    var componentInstances = this.componentInstances;
     xtag.register(tagName, {
         // extend existing elements
         extends: extendsTag,
@@ -17,6 +16,7 @@ Lotus.ComponentMap.prototype.mapComponent = function( tagName, extendsTag, funct
                 // is initially created or parsed
                 if( this.lotusComponentInstance === null || this.lotusComponentInstance === undefined ){
                     this.lotusComponentInstance  = new functionConstructor();
+                    this.componentInstances.addItem(this.lotusComponentInstance);
                 }
                 this.lotusComponentInstance.created(this);
                 var componentInstance = this.lotusComponentInstance;
