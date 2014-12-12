38b71d2b842540425716f796b699b26599a8c53f
dorian smiley <doriansmiley@siliconpublishing.com>
2014-12-12 10:45:35 -0800

    updating test

--- test/unit/AbstractComponentTest.js
+++ test/unit/AbstractComponentTest.js
@@ -9,7 +9,7 @@ describe('AbstractComponentTest', function () {
     it('check AbstractComponent function and values', function () {
         var component = new Lotus.AbstractComponent();
         component.testProperty = null;
-        component.skinParts.addItem(new Lotus.SkinPart('testSkinPart', this, 'testProperty'));
+        component.skinParts.addItem(new Lotus.SkinPart('testSkinPart', component, 'testProperty'));
         var element = {};
         var context = new Lotus.Context(Lavender.ModelLocator.getInstance().config);
         component.created(element, context);
