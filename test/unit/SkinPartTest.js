addd2143d80bdd63a199d22a68acd27eacab6769
dorian smiley <doriansmiley@siliconpublishing.com>
2014-12-12 11:25:58 -0800

    finishing view tests

---
+++ test/unit/SkinPartTest.js
@@ -0,0 +1,23 @@
+/**
+ * Created by dsmiley on 1/13/14.
+ */
+'use strict';
+
+/* jasmine specs for controllers go here */
+describe('AbstractComponentTest', function () {
+
+    it('check AbstractComponent function and values', function () {
+        var component = new Lotus.AbstractComponent();
+        component.testProperty = null;
+        var skinPart = new Lotus.SkinPart('testSkinPart', component, 'testProperty');
+        expect(skinPart.label).toBe('testSkinPart');
+        component.skinParts.addItem(skinPart);
+        var element = {};
+        var context = new Lotus.Context(Lavender.ModelLocator.getInstance().config);
+        component.created(element, context);
+        //call add skin part on the component to set the element property of the skin part
+        component.addSkinPart('testSkinPart', element);
+        expect(skinPart.element === element ).toBe(true);
+
+    });
+});
