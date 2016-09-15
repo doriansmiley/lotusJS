381f3dad2d43539f24caf3d32c45b505c9f4eccd
dorian smiley <doriansmiley@yahoo.com>
2016-09-15 11:28:48 -0700

    refactor config and injectors

--- example/sampleApp/js/app/Context.js
+++ example/sampleApp/js/app/Context.js
@@ -23,19 +23,12 @@ SampleApp.Context.prototype.mapCommands = function(){
 
 SampleApp.Context.prototype.mapObjects = function(){
     this.injector.mapObject(SampleApp.HTTP_SERVICE_KEY, SampleApp.HttpServiceFactory.getInstance().getHttpServiceForInjection(this.config));
-    this.injector.mapObject(SampleApp.IMAGE_ASSETS_PARSER_KEY, SampleApp.ImageAssetsParser);
     //Map singletons
-    this.injector.mapSingletonInstance(SampleApp.SERVICE_RESULT_PARSER_KEY, new SampleApp.ServiceResultParser());
+    this.injector.mapSingletonInstance(SampleApp.SERVICE_RESULT_PARSER_KEY, SampleApp.SerializeFactory.getInstance().getServiceResultParser(this.config));
+    this.injector.mapSingletonInstance(SampleApp.SERIALIZE_FACTORY_KEY, SampleApp.SerializeFactory.getInstance());
     this.injector.mapSingletonInstance(SampleApp.APP_SERVICES, new SampleApp.SampleService(this.config));
     this.injector.mapSingletonInstance(SampleApp.EVENT_DISPATCHER_KEY, Lotus.EventDispatcherFactory.getInstance().getEventDispatcher( this.config ));
-    var recordSet = new Lavender.RecordSet();
-    recordSet.id = '1234';
-    recordSet.selectedPage = 1;
-    recordSet.createdOn = new Date();//Date;;
-    recordSet.timeToLive = 500000;
-    recordSet.source = this.config.baseUrl + model.config.serviceMap['readImageAssets'];
-    recordSet.recordsPerPage = 2;
-    this.injector.mapSingletonInstance(SampleApp.IMAGES_KEY, recordSet);
+    this.injector.mapSingletonInstance(SampleApp.MODEL_KEY, new SampleApp.Model());
 }
 
 SampleApp.Context.prototype.mapMediators = function(){
@@ -46,5 +39,6 @@ SampleApp.IMAGE_ASSETS_PARSER_KEY = 'imageAssetsParser';
 SampleApp.HTTP_SERVICE_KEY = 'xhr';
 SampleApp.APP_SERVICES = 'appServices';
 SampleApp.SERVICE_RESULT_PARSER_KEY = 'serviceResultParser';
+SampleApp.SERIALIZE_FACTORY_KEY = 'serializeFactoryKey';
 SampleApp.EVENT_DISPATCHER_KEY = 'eventDispatcher';
-SampleApp.IMAGES_KEY = 'images';
+SampleApp.MODEL_KEY = 'images';
