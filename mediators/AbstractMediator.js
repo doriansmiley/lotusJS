284b08e5379fce893e827e95adfe5b24e5cfb212
dorian smiley <doriansmiley@siliconpublishing.com>
2016-09-15 20:46:25 -0700

    Create abstract mediator

--- mediators/AbstractMediator.js
+++ mediators/AbstractMediator.js
@@ -1,3 +1,66 @@
 /**
  * Created by dsmiley on 9/15/16.
  */
+Lotus.AbstractMediator = function (componentInstance) {
+    var _id = Lavender.UuidUtils.generateUUID();
+    var _componentInstance = componentInstance;
+    // Define our getters and setters
+    this.addProperties({
+            id: {
+                get: function () {
+                    return _id;
+                },
+                set: function (val) {
+                    _id = val;
+                    this.Notify(val, 'id');
+                }
+            },
+            componentInstance: {
+                get: function () {
+                    return _componentInstance;
+                },
+                set: function (val) {
+                    _componentInstance = val;
+                    this.Notify(val, 'componentInstance');
+                }
+            }
+        }
+    );
+    Lavender.Subject.prototype.constructor.call(this);
+    this.init();
+}
+/************* Inherit from AbstractEventDispatcher for event dispatching *************/
+Lavender.ObjectUtils.extend(Lavender.Subject, Lotus.AbstractMediator);
+
+Lotus.AbstractMediator.prototype.init = function () {
+    this.addEventListeners();
+    this.setUpBindings();
+}
+
+//stub for override
+Lotus.AbstractMediator.prototype.addEventListeners = function () {
+
+}
+//stub for override
+Lotus.AbstractMediator.prototype.setUpBindings = function () {
+
+}
+//stub for override
+Lotus.AbstractMediator.prototype.removeEventListeners = function () {
+
+}
+
+Lotus.AbstractMediator.prototype.removeBindings = function () {
+    this.binder.unbindAll();
+}
+
+//IMPORTANT: we must override toString so the hashes in mediator map work properly
+//read: http://stackoverflow.com/questions/194846/is-there-any-kind-of-hash-code-function-in-javascript
+Lotus.AbstractMediator.prototype.toString = function () {
+    return this.id;
+}
+
+Lotus.AbstractMediator.prototype.destroy = function () {
+    this.removeEventListeners();
+    this.removeBindings();
+}
