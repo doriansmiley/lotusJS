71f2efb1bdbab1d81b3143198f3cf25a1dfa92b3
dorian smiley <doriansmiley@siliconpublishing.com>
2016-09-19 20:41:20 -0700

    pass args to prototype

--- mediators/ButtonMediator.js
+++ mediators/ButtonMediator.js
@@ -2,7 +2,7 @@
  * Created by dsmiley on 9/15/16.
  */
 Lotus.ButtonMediator = function (componentInstance, context) {
-    Lotus.AbstractMediator.prototype.constructor.call(this);
+    Lotus.AbstractMediator.prototype.constructor.call(this, componentInstance, context);
 }
 /************* Inherit from AbstractEventDispatcher for event dispatching *************/
 Lavender.ObjectUtils.extend(Lotus.AbstractMediator, Lotus.ButtonMediator);
