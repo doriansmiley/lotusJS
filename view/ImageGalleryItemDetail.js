e259ed3528ee569bc8fa7efac783e521e0ab0b2f
dorian smiley <doriansmiley@siliconpublishing.com>
2016-10-18 21:06:38 -0700

    add test, refactor based on testing

--- view/ImageGalleryItemDetail.js
+++ view/ImageGalleryItemDetail.js
@@ -1,28 +1,49 @@
 /**
  * Created by dsmiley on 10/18/16.
  */
-Lotus.ImageGalleryItemDetail = function(){
+Lotus.ImageGalleryItemDetail = function () {
     //Define private vars
-    var _item;
-    var _label;
+    var _asset;
+    var _nameLabel;
+    var _dateCreatedLabel;
+    var _urlLabel;
     // Define our getters and setters
     this.addProperties({
-        item: {
-                get: function() {
-                    return _item;
+            asset: {
+                get: function () {
+                    return _asset;
                 },
-                set: function(val) {
-                    _item = val;
-                    this.Notify( val, 'item' );
+                set: function (val) {
+                    _asset = val;
+                    this.Notify(val, 'asset');
+                    this.render();
                 }
             },
-        label: {
-                get: function() {
-                    return _label;
+            nameLabel: {
+                get: function () {
+                    return _nameLabel;
                 },
-                set: function(val) {
-                    _label = val;
-                    this.Notify( val, 'label' );
+                set: function (val) {
+                    _nameLabel = val;
+                    this.Notify(val, 'nameLabel');
+                }
+            },
+            dateCreatedLabel: {
+                get: function () {
+                    return _dateCreatedLabel;
+                },
+                set: function (val) {
+                    _dateCreatedLabel = val;
+                    this.Notify(val, 'dateCreatedLabel');
+                }
+            },
+            urlLabel: {
+                get: function () {
+                    return _urlLabel;
+                },
+                set: function (val) {
+                    _urlLabel = val;
+                    this.Notify(val, 'urlLabel');
                 }
             }
         }
@@ -32,25 +53,37 @@ Lotus.ImageGalleryItemDetail = function(){
 /************* Inherit from Lotus.AbstractComponent for data binding *************/
 Lavender.ObjectUtils.extend(Lotus.AbstractComponent, Lotus.ImageGalleryItemDetail);
 
-Lotus.ImageGalleryItemDetail.prototype.defineSkinParts = function(){
+Lotus.ImageGalleryItemDetail.prototype.defineSkinParts = function () {
     //set up skin parts
-    this.skinParts.addItem(new Lotus.SkinPart('label', this, 'label'));
+    this.skinParts.addItem(new Lotus.SkinPart('nameLabel', this, 'nameLabel'));
+    this.skinParts.addItem(new Lotus.SkinPart('dateCreatedLabel', this, 'dateCreatedLabel'));
+    this.skinParts.addItem(new Lotus.SkinPart('urlLabel', this, 'urlLabel'));
 }
 
-Lotus.ImageGalleryItemDetail.prototype.created = function(element){
+Lotus.ImageGalleryItemDetail.prototype.created = function (element) {
     console.log('Lotus.ImageGalleryItemDetail.prototype.created');
-    Lotus.AbstractComponent.prototype.created.call(this,element);
+    Lotus.AbstractComponent.prototype.created.call(this, element);
+    this.render();
 }
 
-//stub for override
-Lotus.ImageGalleryItemDetail.prototype.onSkinPartAdded = function(part, skinPart){
-    switch( part ){
-        case 'button':
-            //add button event listener or whatever else yo want to do when this skin part is added
-            //you could hold until all skin parts are added and then call addEventListeners
-            console.log('Lotus.ImageGalleryItemDetail.prototype.onSkinPartAdded: part: ' + part);
-            console.log('Lotus.ImageGalleryItemDetail.prototype.onSkinPartAdded: skinPart: ' + skinPart);
-            this.addEventListeners();
-            break;
+Lotus.ImageGalleryItemDetail.prototype.render = function () {
+    if (this.asset && this.ready) {
+        if(this.nameLabel){
+            this.nameLabel.value = this.asset.objectName;
+        }
+        if(this.dateCreatedLabel){
+            this.dateCreatedLabel.value = this.asset.createdDate.toDateString();
+        }
+        if(this.urlLabel){
+            this.urlLabel.value = this.asset.url;
+        }
     }
+}
+
+Lotus.ImageGalleryItemDetail.prototype.destroy = function(){
+    Lotus.AbstractComponent.prototype.destroy.call(this);
+    this.asset = null;
+    this.nameLabel = null;
+    this.dateCreatedLabel = null;
+    this.urlLabel = null;
 }
