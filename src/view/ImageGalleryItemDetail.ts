5b554d36e462a9e5e5eb2e8ad58270fd2e261736
dorian smiley <doriansmiley@yahoo.com>
2017-08-24 17:47:57 -0700

    add gallery components

---
+++ src/view/ImageGalleryItemDetail.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by dsmiley on 8/24/17.
+ */
+import * as Lavender from 'lavenderjs/lib';
+import {SkinPart} from "./SkinPart";
+import {AbstractComponent} from "./AbstractComponent";
+
