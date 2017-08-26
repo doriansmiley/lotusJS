1aaa5c24be3d416cff8c6a731572b4f9be527669
dorian smiley <doriansmiley@yahoo.com>
2017-08-25 17:23:42 -0700

    remove node modules

--- example/sampleApp/node_modules/lotusjs-components/lib/control/events/ActionSuccessEvent.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dsmiley on 5/18/17.
- */
-import * as Lavender from 'lavenderjs/lib';
-export declare class ActionSuccessEvent extends Lavender.AbstractEvent {
-    constructor(type: string, payload?: Object);
-    static SUCCESS: string;
-    clone(type: string, payload: Object): Lavender.IEvent;
-}
