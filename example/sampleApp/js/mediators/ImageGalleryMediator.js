6c69f581fa8f110444d5bbe6143079509dfe5c05
dorian smiley <doriansmiley@yahoo.com>
2017-08-28 12:44:12 -0700

    refactor based on testing, compile, publish

--- example/sampleApp/js/mediators/ImageGalleryMediator.js
+++ example/sampleApp/js/mediators/ImageGalleryMediator.js
@@ -24,7 +24,7 @@ SampleApp.ImageGalleryMediator.prototype.init = function () {
     var model = this.context.injector.inject(SampleApp.MODEL_KEY);
     if( model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel] === null || model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel] === undefined ){
         //create the record set for the source if it's not already defined
-        model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel] = new Lavender.RecordSet(null, Lavender.ArrayList);
+        model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel] = new Lavender.RecordSet(NaN, Lavender.ArrayList);
         model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel].createdOn = new Date();
         model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel].id = Lavender.UuidUtils.generateUUID();
         model.recordsetModel.recordSets.recordSetsBySource[recordSetLabel].recordsPerPage = model.config.galleryItemsPerPage;
