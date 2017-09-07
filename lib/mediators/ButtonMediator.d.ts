19cf6f01c6b0efed6550fb6fd5af9083e2e027ef
dorian smiley <doriansmiley@yahoo.com>
2017-09-06 21:52:14 -0700

    remove injections from button mediator

--- lib/mediators/ButtonMediator.d.ts
+++ lib/mediators/ButtonMediator.d.ts
@@ -1,12 +1,10 @@
 import { AbstractMediator } from "./AbstractMediator";
 import { IContext } from "../context/IContext";
 import { IComponent } from "../view/IComponent";
-import { HttpServiceFactory } from "../factory/HttpServiceFactory";
 /**
  * Created by dsmiley on 7/26/17.
  */
 export declare class ButtonMediator extends AbstractMediator {
-    serviceFactory: HttpServiceFactory;
     constructor(componentInstance: IComponent, context: IContext);
     protected onClick(event: Event): void;
     protected addEventListeners(): void;
--- src/mediators/ButtonMediator.ts
+++ src/mediators/ButtonMediator.ts
@@ -1,16 +1,21 @@
 import {AbstractMediator} from "./AbstractMediator";
 import {IContext} from "../context/IContext";
 import {IComponent} from "../view/IComponent";
+import {inject} from "../reflection/InjectorDecorator";
+import {HttpServiceFactory} from "../factory/HttpServiceFactory";
 /**
  * Created by dsmiley on 7/26/17.
  */
+
 export class ButtonMediator extends AbstractMediator{
+
     constructor(componentInstance:IComponent, context:IContext){
+        console.log('ButtonMediator constructor called')
         super(componentInstance,context);
     }
 
     protected onClick(event:Event):void{
-        console.log('Im the button mediator, I can handle the component click and dispatch an application event.')
+        console.log('Im the button mediator, I can handle the component click and dispatch an application event.');
     }
 
     protected addEventListeners():void{
