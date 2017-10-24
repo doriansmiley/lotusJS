b67d68e5ff6b94baa2f01a551e1bfa3547b8fa9c
dorian smiley <doriansmiley@yahoo.com>
2017-10-23 22:13:47 -0700

    add new x-tag v2 test config
    trying to get out in front of x-tag changes

---
+++ test/config/x-tag-vs-test-karma.conf.js
@@ -0,0 +1,42 @@
+module.exports = function (config) {
+    config.set({
+        basePath: '../',
+
+        files: [
+            //'../node_modules/x-tag/lib/webcomponents.js',
+            //'../node_modules/x-tag/dist/x-tag-core-with-shadowdom.min.js',
+            '../../x-tag/dist/x-tag-polyfilled.js',
+            '../node_modules/lavenderjs/lib/lavenderJS-UMD.min.js',
+            '../lib/lotusJS-UMD.min.js',
+            '../global/Globals.js',
+            {pattern: 'unit/assets/**', watched: false, included: false, served: true},
+            {pattern: 'unit/templates/**', watched: false, included: false, served: true},
+            'unit/**/*.js'
+        ],
+
+        proxies: {
+            "/local": "http://localhost"
+        },
+
+        exclude: ['lib/angular/angular-scenario.js'],
+
+        autoWatch: true,
+
+        frameworks: ['jasmine'],
+
+        browsers: ['Chrome'],
+
+        plugins: [
+            'karma-junit-reporter',
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine'
+        ],
+
+        junitReporter: {
+            outputFile: 'test_out/unit.xml',
+            suite: 'unit'
+        }
+
+    })
+}
