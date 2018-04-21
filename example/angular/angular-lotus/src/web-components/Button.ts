dbe1c55acfda20654062551bcae1046a70faa341
dorian smiley <doriansmiley@yahoo.com>
2018-04-20 22:44:43 -0700

    add data binding example

--- example/angular/angular-lotus/src/web-components/Button.ts
+++ example/angular/angular-lotus/src/web-components/Button.ts
@@ -3,29 +3,44 @@
  */
 import { Component } from '@angular/core';
 import {Button} from 'lotusjs-components/lib/view/Button';
+import {ButtonModel} from '../model/ButtonModel'
 import { ViewEncapsulation, AfterViewInit, Renderer, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'x-lotus-button',
   templateUrl: './Button.html',
-  styleUrls: ['./Button.css']
+  styleUrls: ['./Button.css'],
+  providers:[ButtonModel]
 })
 export class AngularButton extends Button implements AfterViewInit{
 
   @ViewChild('myButton')
   button:ElementRef;
 
-  constructor(){
+  constructor(private buttonModel:ButtonModel){
     super();
+    //set up bindings using Lotus bindings utils instead of Angular.
+    this.binder.bind(this.buttonModel, 'label', this, 'onLableChange');
+  }
+
+  public onLableChange(value:string):void{
+    this.element.innerHTML = value;
+  }
+
+  public onClick(event:Event):void{
+    super.onClick(event);
+    //here we reset the model values which will trigger a change in the view. Pretty cool!!!
+    this.buttonModel.label = 'hey, keep your pointer to yourself';
   }
 
   public ngAfterViewInit():void {
     this.element = this.button.nativeElement;
     this.init();
+    this.onLableChange(this.buttonModel.label);//must init the value as bindings don't trigger when created
   }
 
   public ngOnDestroy():void{
-    this.destroy();
+    this.destroy();//will clear all bindings
   }
 
   //TODO: implement a data binding example. The Angular component acts as a mediator
