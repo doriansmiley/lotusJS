c304beb62841b2e820070f04f940de193ba394ff
dorian smiley <doriansmiley@yahoo.com>
2019-04-25 07:12:02 -0700

    remove generated files

--- lib/control/events/InputEvent.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by dsmiley on 9/21/17.
- */
-import * as Lavender from 'lavenderjs/lib';
-export declare class InputEvent extends Lavender.AbstractEvent {
-    constructor(type: string, payload?: Object);
-    static CHANGE: string;
-    clone(type: string, payload: Object): Lavender.IEvent;
-}
--- lib/model/form/validation/PhoneNumberValidator.d.ts
+++
@@ -1,9 +0,0 @@
-import { TextInputValidator } from "./TextInputValidator";
-import * as Lavender from 'lavenderjs/lib';
-/**
- * Created by dsmiley on 10/10/17.
- */
-export declare class PhoneNumberValidator extends TextInputValidator {
-    constructor();
-    protected getValidationErrors(): Lavender.ArrayList;
-}
