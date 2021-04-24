5b9299ce858330159b74f36aaedf9f331a247cd6
doriansmiley <doriansmiley@siliconpublishing.com>
2021-04-23 18:13:51 -0700

    add dockerfile for puppetteer

--- example/ssr-cluster/server.js
+++ example/ssr-cluster/server.js
@@ -5,15 +5,13 @@ const path = require('path');
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cors = require('cors');
-const port = process.argv[2] || 3000;
-const dockerPort = process.argv[3] || 3001;
+const port = process.argv[2] || 3001;
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const ssr = require('./ssr/ssr');
+const ssr = require('./ssr');
 
 const corsOptions = {
     origin: [
-        `http://localhost:${port}`,
-        `http://localhost:${dockerPort}`,
+        `http://localhost:${port}`
     ],
     credentials: true
 };
@@ -29,23 +27,6 @@ app.get('/ssr', async (req, res, next) => {
     // Add Server-Timing! See https://w3c.github.io/server-timing/.
     res.set('Server-Timing', `Prerender;dur=${ttRenderMs};desc="Headless render time (ms)"`);
     return res.status(200).send(html); // Serve prerendered page as response.
-});
-
-app.get('/sample', async (req, res, next) => {
-    // TODO replace hard coded value
-    console.log(req.query.url);
-    console.log(req.query.selector);
-    await new Promise((resolve) => {
-        setTimeout(() => resolve(), 1000);
-    });
-    return res.status(200).send([
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/0.jpg', caption: '<h5>0.jpg</h5>'},
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/6.jpg', caption: '<h5>1.jpg</h5>'},
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/2.jpg', caption: '<h5>2.jpg</h5>'},
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/3.jpg', caption: '<h5>3.jpg</h5>'},
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/4.jpg', caption: '<h5>4.jpg</h5>'},
-        {name: '', src: 'http://localhost:3000/image-gallery/assets/5.jpg', caption: '<h5>5.jpg</h5>'},
-    ]);
 });
 
 app.use('/', express.static(path.join(__dirname,'.')));
---
+++ example/ssr-cluster/ssr.js
@@ -0,0 +1,129 @@
+const puppeteer = require('puppeteer');
+
+// TODO: add job dao to get publishing jobs via some factory. Note that
+//  implementations can include Kafka, Kenisis, RabbitMQ, etc,
+//  bue we opted for mongo because of several years experience with
+//  this architecture as well as the fact Atlas supports multi cloud architecture
+//  poll mongoDB by obtaining get the connection details using a connection factory.
+//  Pass an ENV var to the factory which will be something like gcp, aws, localhost
+//  code an example for local host but included code examples for using AWS SSM parameter store
+//  the localhost example should access ENV vars, but not ssm or similar is way more secure
+
+// https://hackernoon.com/tips-and-tricks-for-web-scraping-with-puppeteer-ed391a63d952
+// Dont download all resources, we just need the HTML
+// Also, this is huge performance/response time boost
+const blockedResourceTypes = [
+    'image',
+    'media',
+    'font',
+    'texttrack',
+    'object',
+    'beacon',
+    'csp_report',
+    'imageset',
+];
+
+const skippedResources = [
+    'quantserve',
+    'adzerk',
+    'doubleclick',
+    'adition',
+    'exelator',
+    'sharethrough',
+    'cdn.api.twitter',
+    'google-analytics',
+    'googletagmanager',
+    'google',
+    'fontawesome',
+    'facebook',
+    'analytics',
+    'optimizely',
+    'clicktale',
+    'mixpanel',
+    'zedo',
+    'clicksor',
+    'tiqcdn',
+];
+
+const RENDER_CACHE = new Map();
+
+/**
+ * https://developers.google.com/web/tools/puppeteer/articles/ssr#reuseinstance
+ * @param {string} url URL to prerender.
+ * @param {string} browserWSEndpoint Optional remote debugging URL. If
+ *     provided, Puppeteer's reconnects to the browser instance. Otherwise,
+ *     a new browser instance is launched.
+ */
+async function ssr (url, browserWSEndpoint, selector) {
+    if (RENDER_CACHE.has(url)) {
+        return {html: RENDER_CACHE.get(url), ttRenderMs: 0};
+    }
+
+    const start = Date.now();
+    const args = puppeteer.defaultArgs();
+    // IMPORTANT: you can't render shadow DOM without this flag
+    // getInnerHTML will be undefined without it
+    args.push('--enable-experimental-web-platform-features');
+    const browser = await puppeteer.launch({
+        args
+    });
+    const page = await browser.newPage();
+    try {
+        await page.setRequestInterception(true);
+
+
+        page.on('request', request => {
+            const requestUrl = request._url.split('?')[0].split('#')[0];
+            if (
+                blockedResourceTypes.indexOf(request.resourceType()) !== -1 ||
+                skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
+            ) {
+                request.abort();
+            } else {
+                request.continue();
+            }
+        });
+
+        // Inject <base> on page to relative resources load properly.
+        await page.evaluate(url => {
+            const base = document.createElement('base');
+            base.href = url;
+            // Add to top of head, before all other resources.
+            document.head.prepend(base);
+        }, url);
+
+        await page.goto(url, {
+            timeout: 25000,
+            waitUntil: 'networkidle0'
+        });
+
+
+        await page.waitForFunction(selector => !!document.querySelector(selector)?.shadowRoot, {
+            polling: 'mutation',
+        }, selector);
+
+        let html = await page.$eval('html', (element, tagname) => {
+            return element.getInnerHTML({includeShadowRoots: true});
+        }, selector);
+
+        // Close the page we opened here (not the browser).
+        await page.close();
+        // TODO figure out why the base element is stripped from serialization
+        html = html.replace('<head>', `<head><base href="${url}"/>`);
+
+        const ttRenderMs = Date.now() - start;
+        console.info(`Headless rendered page in: ${ttRenderMs}ms`);
+
+        RENDER_CACHE.set(url, html); // cache rendered page.
+
+        return {html, ttRenderMs};
+    }
+    catch (e) {
+        const html = e.toString();
+        console.warn({ message: `URL: ${url} Failed with message: ${html}` });
+        return { html, status: 500 };
+    }
+
+}
+
+module.exports = ssr;
